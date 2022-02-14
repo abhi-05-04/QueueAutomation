2a0183f80ccdf595f93fd44bebc5fa89882dc705
pavanshinde7494 <pavanshinde7494@gmail.com>
2022-02-14 10:42:09 +0530

    Unique phone number

--- pages/api/registerQ.js
+++ pages/api/registerQ.js
@@ -5,16 +5,24 @@ export default async function handler(req,res){
     const { db } = await connectToDatabase()
     const data  = req.query
     
-    await db.collection('Queue').insertOne(data)
-    .then((result)=>{
-        let id = result.insertedId;
-        console.log(id);
-        res.send(id);
-    }) 
-    .catch((err)=>{
-        res.json({message:"error in database"});
-        console.log(err);
+    console.log(data);
+    await db.collection('Queue').findOne({'phone' : data.phone , 'admin' : data.admin})
+    .then(async(candFound)=>{
+        if(!candFound){
+            await db.collection('Queue').insertOne(data)
+            .then((result)=>{
+                let id = result.insertedId;
+                console.log(id);
+                res.send(id);
+            }) 
+            .catch((err)=>{
+                res.json({message:"error in database"});
+                console.log(err);
+            })
+        }
     })
+
+   
 
 
 }
